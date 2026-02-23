b9c499ab44c343977d686207d29eabbee75efdac
X1106 <x1106ktag@gmail.com>
2026-02-23 19:01:01 +0900

    サ イトの内容を微調整

--- my-fullstack-app/app/api/auth/[...nextauth]/route.ts
+++ my-fullstack-app/app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,7 @@
 import { handlers } from "@/lib/auth";
 
+export const runtime = "nodejs"; // ✅重要
+export const dynamic = "force-dynamic"; // 任意（キャッシュ回避）
+
 export const GET = handlers.GET;
 export const POST = handlers.POST;
